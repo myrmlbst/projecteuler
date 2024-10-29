d878b990154771a8f5d148b07d4d0b90574a4ca2
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-10-29 09:44:18 +0200

    Create 002.evenFibonacciNumbers.cpp

---
+++ solutions/1-100/002.evenFibonacciNumbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int evenfibsum(int limit) {
+    int prev = 1; 
+    int curr = 2;
+    int sum = 2;
+    
+    while (curr <= limit) {
+        curr = prev + curr;
+        prev = curr - prev;
+        
+        if (curr % 2 == 0) {
+            sum += curr;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int limit = 4000000;
+    std::cout << "sum = " << evenfibsum(limit) << std::endl;
+    return 0;
+}
