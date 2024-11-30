9c0e10440e701bc7f7088a9109bd32af6750f4a1
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-11-30 15:41:42 +0200

    sol 004

---
+++ solutions/1-100/004.largestPalindromeProduct.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isPalindrome(int prod) {
+    string str = to_string(prod);
+    reverse(str.begin(),str.end());
+    
+    if (to_string(prod) == str) {
+        return true;
+    }
+    
+    return false;
+}
+
+int main() {
+    int res = -1;
+    
+    for (int i=990; i>99; i-=11) {
+        for (int j=999; j>99; j--) {
+            int prod = i*j;
+          
+            if (prod>res && isPalindrome(prod)) {
+                res = prod;
+                break;
+            }
+            if (prod<res) { 
+                break;
+            }
+        }
+    }
+    
+    cout << res;
+}
